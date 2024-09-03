c9a693a3615c03796647d3ed7f3bee4433ed849d
unknown <colin.neaville@gmail.com>
2024-09-03 14:45:31 -0500

    Added Neumann boundary conditions to Laplace equation demo.

--- fdmlaplace.py
+++ fdmlaplace.py
@@ -1,72 +1,114 @@
+# This program numerically solves the Laplace equation with a source in a rectangular region where each boundary has either Dirichlet or Neumann conditions
+
 import numpy as np
 import scipy.sparse
 import scipy.sparse.linalg
 
 import matplotlib.pyplot as plt
 
-M=100
-N=100
+# M = # nodes in x direction, N = # nodes in y direction
+M = 25
+N = 25
 
-delta_x = 1.0/(N-1.0)
-delta_y = 1.0/(M-1.0)
+delta_x = 1/M
+delta_y = 1/N
 
-A = 1.0/(delta_x**2)
-B = 1.0/(delta_y**2)
-C = -2.0*A - 2.0*B
+A = 1/(delta_x ** 2)
+B = 1/(delta_y ** 2)
+C = -2 * A - 2 * B
 
-def is_boundary_index(k):
-    return (k % N == 0 or k % N == N-1 or k>=(M-1)*N or k<N)
+def source(x,y):
+    return 0
 
-main_diagonal = np.ones(M*N)
-for k in range(M*N):
-    if is_boundary_index(k):
-        main_diagonal[k] = 1
-    else:
+# returns coordinates of a node given its 1d index k
+def get_coords_from_index(k):
+    i = k % M
+    j = k // M
+    return i * delta_x, j * delta_y
+
+# boundary condition types
+boundary_conditions = {'left': 'dirichlet', 'right': 'dirichlet', 'top': 'dirichlet', 'bottom': 'neumann'}
+prescribed_values = {'left': 0.0, 'right': 0.0, 'top':1.0, 'bottom': 0.0}
+
+def get_node_type(k):
+    # get 2d indices of node (i,j) from 1d index k 
+    i = k % M
+    j = k // M
+
+    if i == 0:
+        return 'left'
+    elif i == M - 1:
+        return 'right'
+    elif j == 0:
+        return 'bottom'
+    elif j == N - 1:
+        return 'top'
+    
+    return 'interior'
+    
+# generate matrix of system ========================================
+main_diagonal = np.zeros(M*N)
+for k in range(0,M*N):
+    node_type = get_node_type(k)
+    if node_type == 'interior':
         main_diagonal[k] = C
+    elif boundary_conditions[node_type] == 'dirichlet':
+        main_diagonal[k] = 1
+    elif node_type == 'left' or node_type == 'right':
+        main_diagonal[k] = -1/delta_x
+    elif node_type == 'top' or node_type == 'bottom':
+        main_diagonal[k] = -1/delta_y
 
-a_diagonal_left = np.zeros(M*N-1)
-for k in range(1,M*N):
-    l = k-1 #index of entry in diagonal
-    if not is_boundary_index(k):
-        a_diagonal_left[l] = A
+plus_one_diagonal = np.zeros(M*N-1)
+for k in range(0, M*N-1):
+    node_type = get_node_type(k)
+    if node_type == 'interior':
+        plus_one_diagonal[k] = A
+    if node_type == 'left' and boundary_conditions[node_type] == 'neumann':
+        plus_one_diagonal[k] = 1/delta_x
 
-a_diagonal_right = np.zeros(M*N-1)
-for k in range(0,M*N-1):
-    l = k #index of entry in diagonal
-    if not is_boundary_index(k):
-        a_diagonal_right[l] = A
+minus_one_diagonal = np.zeros(M*N-1)
+for k in range(1, M*N):
+    node_type = get_node_type(k)
+    if node_type == 'interior':
+        minus_one_diagonal[k-1] = A
+    if node_type == 'right' and boundary_conditions[node_type] == 'neumann':
+        minus_one_diagonal[k-1] = 1/delta_x
 
-b_diagonal_left = np.zeros(M*N-N)
-for k in range(N,M*N):
-    l = k-N
-    if not is_boundary_index(k):
-        b_diagonal_left[l] = B
+plus_M_diagonal = np.zeros(M*N-M)
+for k in range(0, M*N-M):
+    node_type = get_node_type(k)
+    if node_type == 'interior':
+        plus_M_diagonal[k] = B
+    if node_type == 'bottom' and boundary_conditions[node_type] == 'neumann':
+        plus_M_diagonal[k] = 1/delta_y
 
-b_diagonal_right = np.zeros(M*N-N)
-for k in range(0,M*N-N):
-    l = k #index of entry in diagonal
-    if not is_boundary_index(k):
-        b_diagonal_right[l] = B
+minus_M_diagonal = np.zeros(M*N-M)
+for k in range(M, M*N):
+    node_type = get_node_type(k)
+    if node_type == 'interior':
+        minus_M_diagonal[k-M] = B
+    if node_type == 'top' and boundary_conditions[node_type] == 'neumann':
+        minus_M_diagonal[k-M] = 1/delta_y
 
+y = np.zeros(M*N)
 
-diags = [main_diagonal, a_diagonal_left, a_diagonal_right, b_diagonal_left, b_diagonal_right]
-diag_positions = [0, -1, 1, -N, N]
+for k in range(M*N):
+    node_type = get_node_type(k)
+    if node_type == 'interior':
+        x_coord,y_coord = get_coords_from_index(k)
+        y[k] = source(x_coord,y_coord)
+    else:
+        y[k] = prescribed_values[node_type]
+
+diags = [main_diagonal, plus_one_diagonal, minus_one_diagonal, plus_M_diagonal, minus_M_diagonal]
+diag_positions = [0, 1, -1, M, -M]
 Mat = scipy.sparse.diags(diags, diag_positions, format='csc')
 
-y = np.ones(M*N)
-
-#k % N == 0 or k % N == N-1 or k>=(M-1)*N or k<N
-
-for k in range(M*N):
-    if is_boundary_index(k):
-        y[k] = 0.0
-    else:
-        y[k] = -1
-
+# solve system Ax = y
 x = scipy.sparse.linalg.spsolve(Mat, y)
 
-print(x)
-
+# render the solution
 from matplotlib import colors
  
 data = x.reshape(M,N)
@@ -81,6 +123,3 @@ fig, ax = plt.subplots()
 pc = ax.pcolormesh(data, cmap=cmap)
 
 plt.show()
-    
-
-
